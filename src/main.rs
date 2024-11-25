b2caf455a1a4eab6c7ce2207b30721a4dd177cdf
ec <ec@localhost.localdomain>
2024-11-25 12:20:00 -0500

    Added index.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+use std::thread;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    let file = warp::path::end().map(|| "Hello this is a server");
+
+    warp::serve(file).run(([192,168,42,213],8443)).await;
 }
